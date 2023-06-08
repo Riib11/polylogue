93b8f2d94564d058c294d7e5c979565767be8039
Henry Blanchette <henry@monkeyface.com>
2023-06-08 14:57:39 +0800

    Manager

--- src/AI/Agent/Manager.purs
+++ src/AI/Agent/Manager.purs
@@ -11,6 +11,7 @@ import Data.Either (Either(..))
 import Data.Functor.Variant as FV
 import Data.Functor.Variant as VF
 import Data.Symbol (class IsSymbol)
+import Data.Tuple (Tuple)
 import Data.Variant as V
 import Hole (hole)
 import Prim.Row (class Cons, class Union)
@@ -23,8 +24,7 @@ _agents = Proxy :: Proxy "agents"
 
 -- | A manager agent maintains a state with labeled sub-agent instances.
 subQuery :: forall subLabel subStates subQueries agents agents_ states errors m a.
-  IsSymbol subLabel =>
-  Cons subLabel (Agent.Agent subStates errors subQueries m /\ Record subStates) agents_ agents =>
+  IsSymbol subLabel => Cons subLabel (Agent.Agent subStates errors subQueries m /\ Record subStates) agents_ agents =>
   Monad m =>
   Proxy subLabel ->
   Agent.QueryInput subQueries a ->
@@ -37,5 +37,16 @@ subQuery subLabel input = do
       Left err -> throwError err
       Right a -> pure a
 
-subInquire subLabel queryLabel input = 
-  subQuery subLabel (FV.inj queryLabel (Agent.Inquiry input identity))
+-- !TODO i dont think i should make anything specified to Inquiries -- should
+-- just be able to be used anywhere generic queries are used
+
+-- subInquire :: forall subLabel subStates subQueries agents agents_ states errors m a queryLabel subQueries_ input.
+--   IsSymbol subLabel => Cons subLabel (Tuple (Agent.Agent subStates errors subQueries m) (Record subStates)) agents_ agents =>
+--   IsSymbol queryLabel => Cons queryLabel (Agent.Inquiry input a) subQueries_ subQueries =>
+--   Monad m =>
+--   Proxy subLabel ->
+--   Proxy queryLabel ->
+--   input ->
+--   Agent.AgentM (States agents states) errors m a
+-- subInquire subLabel queryLabel input = 
+--   subQuery subLabel (FV.inj queryLabel (Agent.Inquiry input identity))
